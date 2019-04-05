41f41c1f856bd9536ec541efb7437b9cee33768f
Maxim Zhiburt <zhiburt@gmail.com>
2019-04-05 21:29:21 +0300

    Add communication structure

---
+++ monitor/communication/message.go
@@ -0,0 +1,9 @@
+package communication
+
+type Message struct {
+	Type      string `json:"type"`
+	UserID    string `json:"id"`
+	Body      []byte `json:"body"`
+	Name      string `json:"name"`
+	Extension string `json:"extension"`
+}
