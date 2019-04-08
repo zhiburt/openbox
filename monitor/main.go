112893c7465bf16135b8a6fb95dcfeba32eefa12
Maxim Zhiburt <zhiburt@gmail.com>
2019-04-08 22:30:10 +0300

    Change main.go
    for service

--- monitor/main.go
+++ monitor/main.go
@@ -1,73 +1,102 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
-	"log"
+	"net/http"
 	"os"
-	"strings"
+	"os/signal"
+	"syscall"
 
-	"github.com/openbox/monitor/qservice"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
-	"github.com/openbox/monitor/communication"
+	"go.mongodb.org/mongo-driver/mongo"
+
+	log "github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
+	"github.com/openbox/monitor/services/monitor"
+	"github.com/openbox/monitor/services/monitor/impl"
+	"github.com/openbox/monitor/services/monitor/repositories"
+	"github.com/openbox/monitor/services/monitor/transport/endpoints"
+	"github.com/openbox/monitor/services/monitor/transport/httptransport"
+	"github.com/openbox/monitor/services/qservice"
 )
 
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Fatalf("%s: %s", msg, err)
+var net = "localhost"
+var httpAddr = ":8082"
+
+func init() {
+	if n := os.Getenv("NETWORKNAME"); n != "" {
+		net = n
+	}
+	if n := os.Getenv("HTTP_ADDR"); n != "" {
+		httpAddr = n
 	}
 }
 
 func main() {
-	var net = "localhost"
-	if n := os.Getenv("NETWORKNAME"); n != "" {
-		log.Println("network", n)
-		net = n
+	var logger log.Logger
+	{
+		logger = log.NewLogfmtLogger(os.Stderr)
+		logger = log.NewSyncLogger(logger)
+		logger = level.NewFilter(logger, level.AllowDebug())
+		logger = log.With(logger,
+			"service", "remote filesystem monitor",
+			"ts", log.DefaultTimestampUTC,
+			"caller", log.DefaultCaller,
+		)
 	}
 
-	body := bodyFrom(os.Args)
+	level.Info(logger).Log("msg", "service started")
+	defer level.Info(logger).Log("msg", "service finished")
 
-	qs, err := qservice.NewQueueService("guest", "guest", net, "task_exchange")
-	if err != nil {
-		log.Println("[erorr] happend", err)
-		os.Exit(1)
+	var db *mongo.Client
+	{
+		var err error
+		db, err = mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+		if err != nil {
+			level.Error(logger).Log("exit", err)
+			os.Exit(-1)
+		}
 	}
 
-	if len(os.Args) > 5 {
-		body, err = qs.Send(context.Background(), body, os.Args[5])
-	} else {
-		body, err = qs.Send(context.Background(), body, "")
+	var qs qservice.QueueService
+	{
+		var err error
+		qs, err = qservice.NewQueueService("guest", "guest", net, "task_exchange")
+		if err != nil {
+			level.Error(logger).Log("exit", err)
+			os.Exit(-1)
+		}
 	}
 
-	if err != nil {
-		log.Println("[erorr] happend", err)
-		os.Exit(1)
-	}
-	log.Printf(" [x] responce from server %s", body)
-}
-
-func bodyFrom(args []string) []byte {
-	m := message(args[1], args[2], args[3], args[4])
-
-	if len(args) > 6 {
-		n := strings.Split(args[6], ".")
-		m.NewName = n[0]
-		m.NewExtension = n[1]
+	// Create Order Service
+	var mservice monitor.Service
+	{
+		repository := repositories.NewRepository(db, &logger)
+		mservice = impl.NewService(repository, logger, qs)
 	}
 
-	fmt.Println(m)
-	b, _ := json.Marshal(m)
-	return b
-}
+	var h http.Handler
+	{
+		endpoints := endpoints.MakeEndpoints(mservice)
+		h = httptransport.NewService(endpoints, logger)
+	}
 
-func message(user, name, body, t string) communication.Message {
-	n := strings.Split(name, ".")
-	mss := communication.Message{}
-	mss.Name = n[0]
-	mss.Extension = n[1]
-	mss.Body = []byte(body)
-	mss.UserID = user
-	mss.Type = t
-	return mss
+	errs := make(chan error)
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	go func() {
+		level.Info(logger).Log("transport", "HTTP", "addr", httpAddr)
+		server := &http.Server{
+			Addr:    httpAddr,
+			Handler: h,
+		}
+		errs <- server.ListenAndServe()
+	}()
+
+	level.Error(logger).Log("exit with", <-errs)
 }
