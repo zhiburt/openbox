9e8501062e24ec969ff1476ce2b63fa62c645266
Maxim Zhiburt <zhiburt@gmail.com>
2019-04-12 09:54:21 +0300

    Add tests for getByOwnerID method
    with ok index
    with negative index
    when services return errors

--- monitor/services/monitor/transport/httptransport/httptransport_test.go
+++ monitor/services/monitor/transport/httptransport/httptransport_test.go
@@ -26,6 +26,7 @@ func TestNewServiceFiles(t *testing.T) {
 		expected           string
 		expectedStatusCode int
 	}{
+		//Create file
 		{
 			repo:               mock.NewRepository("1111", nil, monitor.File{}),
 			qfs:                qmock.NewQcommunicatorMock(nil, nil),
@@ -69,6 +70,52 @@ func TestNewServiceFiles(t *testing.T) {
 			url:                "http://example.com/files",
 			body:               `{"owner_id": "123", "name": "some_name"}`,
 			expected:           `{"error":"try to figure out why"}`,
+			expectedStatusCode: 400,
+		},
+		//Lookup file
+		{
+			repo:               mock.NewRepository("", nil, monitor.File{Name: "1.cpp", OwnerID: "1owner", ServerID: "1"}),
+			qfs:                qmock.NewQcommunicatorMock([]byte("hello world"), nil),
+			method:             "GET",
+			url:                "http://example.com/files/owner/1",
+			body:               ``,
+			expected:           `{"files":[{"owner_id":"1owner","server_id":"1","name":"1.cpp","body":"aGVsbG8gd29ybGQ=","status":""}]}`,
+			expectedStatusCode: 200,
+		},
+		{
+			repo:               mock.NewRepository("", nil, monitor.File{Name: "1.cpp", OwnerID: "1owner", ServerID: "1"}),
+			qfs:                qmock.NewQcommunicatorMock([]byte("hello world"), nil),
+			method:             "GET",
+			url:                "http://example.com/files/owner/-1",
+			body:               ``,
+			expected:           `{"files":[{"owner_id":"1owner","server_id":"1","name":"1.cpp","body":"aGVsbG8gd29ybGQ=","status":""}]}`,
+			expectedStatusCode: 200,
+		},
+		{
+			repo:               mock.NewRepository("", nil, monitor.File{Name: "1.cpp", OwnerID: "1owner"}),
+			qfs:                qmock.NewQcommunicatorMock(nil, nil),
+			method:             "GET",
+			url:                "http://example.com/files/owner/",
+			body:               ``,
+			expected:           `404 page not found`,
+			expectedStatusCode: 404,
+		},
+		{
+			repo:               mock.NewRepository("", nil, monitor.File{Name: "1.cpp", OwnerID: "1owner", ServerID: "1"}),
+			qfs:                qmock.NewQcommunicatorMock(nil, fmt.Errorf("some_error")),
+			method:             "GET",
+			url:                "http://example.com/files/owner/1",
+			body:               ``,
+			expected:           `{"error":"error connect with communication with services"}`,
+			expectedStatusCode: 400,
+		},
+		{
+			repo:               mock.NewRepository("", fmt.Errorf("some_error"), monitor.File{Name: "1.cpp", OwnerID: "1owner"}),
+			qfs:                qmock.NewQcommunicatorMock(nil, nil),
+			method:             "GET",
+			url:                "http://example.com/files/owner/1",
+			body:               ``,
+			expected:           `{"error":"error happend in repository"}`,
 			expectedStatusCode: 400,
 		},
 	}
