b13a0f8e5ea7060d70235be20a1509836b3834e8
Maxim Zhiburt <zhiburt@gmail.com>
2019-04-18 13:57:40 +0300

    Change type of body
    to better communication
    there was problem with parseing body of files

--- monitor/services/monitor/service.go
+++ monitor/services/monitor/service.go
@@ -15,17 +15,29 @@ type Service interface {
 	RemoveByID(ctx context.Context, id string) error
 }
 
+type RawData []byte
+
+func (r RawData) MarshalText() (text []byte, err error) {
+	return r[:], nil
+}
+
+func (r *RawData) UnmarshalText(text []byte) error {
+	*r = text[:]
+
+	return nil
+}
+
 // File represents an file
 type File struct {
-	ID        string `json:"id,omitempty"`
-	OwnerID   string `json:"owner_id"`
-	ServerID  string `json:"server_id"`
-	Name      string `json:"name"`
-	Body      []byte `json:"body,omitempty"`
-	Status    string `json:"status"`
-	CreatedOn int64  `json:"created_on,omitempty"`
-	IsFolder  bool   `json:"is_folder,omitempty"`
-	Files     []File `json:"files,omitempty"`
+	ID        string  `json:"id,omitempty"`
+	OwnerID   string  `json:"owner_id"`
+	ServerID  string  `json:"server_id"`
+	Name      string  `json:"name"`
+	Body      RawData `json:"body,omitempty"`
+	Status    string  `json:"status"`
+	CreatedOn int64   `json:"created_on,omitempty"`
+	IsFolder  bool    `json:"is_folder,omitempty"`
+	Files     []File  `json:"files,omitempty"`
 }
 
 /*
